acc0343e34929e8681112a11ebdc881b1dd11504
elisabichiappini <elisabichiappini@gmail.com>
2024-01-28 15:49:34 +0100

    feat: scaffolding header

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,11 @@
+<header class="container d-flex align-items-center justify-content-between">
+    <div class="ec-logo">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo-dc-comics">
+    </div>
+    <nav class="nav">
+        <a class="nav-link" href="/">Active</a>
+        <a class="nav-link" href="/prodotti">Prodotto</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link disabled">Disabled</a>
+    </nav>
+</header>
