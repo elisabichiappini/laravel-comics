c5ebe64668370e37fc5a94928b56d1537217e050
elisabichiappini <elisabichiappini@gmail.com>
2024-01-29 00:04:29 +0100

    feat: scaffolding favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <!-- favicon -->
+    <link rel="icon" href="/img/favicon.ico" sizes="16x16 24x24 32x32">
+    <!-- /favicon -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DC Comics</title>
     <!--inclusione assets con direttiva vite-->
